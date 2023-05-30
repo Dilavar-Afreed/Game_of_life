cc2820b864237724a337ec8f85ca81b50e5328e1
Dilavar-Afreed <dilavar4598@gmail.com>
2023-05-30 15:00:10 +0530

    GREEN:- test1 passed (test_display_grid)

--- test_glife.py
+++ test_glife.py
@@ -1,9 +1,10 @@
 import glife
 
 def test_display_grid_nthg():
-    assert glife.display_grid([["#","#","#",],["#","#","#"],["#","#","#"]]) == """###
-###
-###"""
+    assert glife.display_grid([["#","#","#",],["#","#","#"],["#","#","#"]]) == """# # # 
+# # # 
+# # # 
+"""
 
                                                                               
                                                                               
