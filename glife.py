5d8b84c78a8aaaa50a368b4fe40e593dbd153de5
Dilavar-Afreed <dilavar4598@gmail.com>
2023-05-30 14:55:12 +0530

    RED:-Assertion error

---
+++ glife.py
@@ -0,0 +1,13 @@
+def display_grid(a):
+    strng=""
+    for i in a:
+        for j in i:
+            strng+=f'{j} '
+        strng+='\n'
+    return strng
+
+    
+    
+    
+    
+print(display_grid([[1,2,3],[1,2,3],[1,2,3]]))
---
+++ test_glife.py
@@ -0,0 +1,9 @@
+import glife
+
+def test_display_grid_nthg():
+    assert glife.display_grid([["#","#","#",],["#","#","#"],["#","#","#"]]) == """###
+###
+###"""
+
+                                                                              
+                                                                              
