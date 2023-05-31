d467ee8775b58a12cec8327efb04521353b74df7
Dilavar-Afreed <dilavar4598@gmail.com>
2023-05-31 08:55:53 +0530

    RED: Assertion error test 2 failed

--- test_glife.py
+++ test_glife.py
@@ -1,10 +1,13 @@
 import glife
 
-def test_display_grid_nthg():
-    assert glife.display_grid([["#","#","#",],["#","#","#"],["#","#","#"]]) == """# # # 
+def test_display_grid():
+    assert glife.display_grid([["#","#","#"],["#","#","#"],["#","#","#"]]) == """# # # 
 # # # 
 # # # 
 """
 
-                                                                              
+def test_display_alive():
+    assert glife.display_grid([[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]]) == '# # # # # \n# # # # # \n# # # # # \n# # # # # \n# # # # #'
+
+                          
                                                                               
